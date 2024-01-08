ba2bc161d4b988a792e0a7d3f7779a5ccb3434d3
KingP1g <aiden.decastro@epn.edu.ec>
2024-01-07 22:53:38 -0500

    commit for tests

---
+++ 3. Coding/Traductor/src/main/java/Logica/Test/Version01/TraductorSanscritoTest.java
@@ -0,0 +1,16 @@
+package Logica.Test.Version01;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TraductorSanscritoTest {
+
+    @Test
+    public void testTraducirPalabraDirecta() {
+        TraductorSanscrito traductor = new TraductorSanscrito();
+        assertEquals("Postura de la Montaña", traductor.traducir("tadasana"));
+        assertEquals("abajo", traductor.traducir("adho"));
+        assertEquals("cadáver", traductor.traducir("shava"));
+    }
+
+    @Test
---
+++ 3. Coding/Traductor/src/main/java/Logica/Test/verson03/TraductorSanscritoTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class TraductorSanscritoTest {
+
+    @Test
+    public void testTraducirContraccion() {
+        TraductorSanscrito traductor = new TraductorSanscrito();
+        String traduccion = traductor.traducir("adho mukha svanasana");
+        assertEquals("Perro mirando hacia abajo", traduccion);
+    }
+
+    @Test
